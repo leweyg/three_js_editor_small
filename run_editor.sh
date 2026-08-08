7a0196e3513ce978eac337a9f58e386d44c4c6d6
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2026-08-08 16:19:56 -0700

    Change hosting to parent folder, so that we can clone other gits next to it.

--- run_editor.sh
+++ run_editor.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # start browser before the server (async)
-open http://localhost:5678/
+open http://localhost:5678/three_js_editor_small/
 
 echo "$(dirname "$0")/"
 pwd
+
+# /Users/leweyg/localfs/gits/three_js_editor_small/run_editor.sh
+
+cd ~/localfs/gits/
 
 # pass control to php:
 php -S localhost:5678
